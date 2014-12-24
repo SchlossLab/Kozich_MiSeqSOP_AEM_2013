d5f72b63a36f0af9a8291e22652a3e6a92e92891
Pat Schloss <pschloss@umich.edu>
2014-12-24 11:55:35 -0500

    Fix path issues for the report file and rm command

--- code/single_read_analysis.sh
+++ code/single_read_analysis.sh
@@ -23,44 +23,31 @@
 FASTA=$1
 QUAL=$(echo $FASTA | sed -e s/fasta/qual/)
 
-FINAL_FASTA=$(echo $FASTA | sed -e s/raw/process/)
+#need to get the paths
+RAW_PATH=$(echo $FASTA | sed -e s/Mock.*fasta//)
+PROCESS_PATH=$(echo $RAW_PATH | sed -e s/raw/process/)
+FILE_STUB=$(echo $FASTA | sed -e 's/.*\/\(Mock.*\).fasta/\1/')
+PROCESS_STUB=$PROCESS_PATH$FILE_STUB
 
-#need to get path to this folder
-OUTPUT_PATH=$(echo $FINAL_FASTA | sed -e s/Mock.*fasta//)
-mkdir -p $OUTPUT_PATH
+mkdir -p $PROCESS_PATH
 
 if [[ $FASTA == *"R1"* ]]; then
-    mothur "#set.dir(output=$OUTPUT_PATH);
+    mothur "#set.dir(output=$PROCESS_PATH);
         align.seqs(fasta=$FASTA, reference=data/references/HMP_MOCK.align, processors=12);
         filter.seqs(fasta=current-data/references/HMP_MOCK.align, vertical=T);
-        seq.error(fasta=current, qfile=$QUAL, report=current, reference=data/references/HMP_MOCK.filter.fasta);"
+        seq.error(fasta=current, qfile=$QUAL, report=$PROCESS_STUB.align.report, reference=$PROCESS_STUB.HMP_MOCK.filter.fasta);"
+
+        rm $PROCESS_STUB.align
+        rm $PROCESS_STUB.align.report
+        rm $PROCESS_STUB.align.filter.fasta
 else
-    mothur "#set.dir(output=$OUTPUT_PATH);
+    mothur "#set.dir(output=$PROCESS_PATH);
         reverse.seqs(fasta=$FASTA, qfile=$QUAL);
         align.seqs(fasta=current, reference=data/references/HMP_MOCK.align, processors=12);
         filter.seqs(fasta=current-data/references/HMP_MOCK.align, vertical=T);
-        seq.error(fasta=current, qfile=current, report=current, reference=data/references/HMP_MOCK.filter.fasta);"
+        seq.error(fasta=current, qfile=$QUAL, report=$PROCESS_STUB.rc.align.report, reference=$PROCESS_STUB.HMP_MOCK.filter.fasta);"
+
+        rm $PROCESS_STUB.rc.align
+        rm $PROCESS_STUB.rc.align.report
+        rm $PROCESS_STUB.rc.align.filter.fasta
 fi
-
-rm $(echo $FINAL_FASTA | sed -e s/fasta/align/)
-rm $(echo $FINAL_FASTA | sed -e s/fasta/filter.fasta/)
-rm $(echo $FINAL_FASTA | sed -e s/fasta/align.report/)
-
-
-#seq.error(fasta=current, qfile=current, report=current, reference=data/references/HMP_MOCK.filter.fasta)
-#Using data/process/121203/Mock1_S1_L001_R2_001.rc.filter.fasta as input file for the fasta parameter.
-#Using data/process/121203/Mock1_S1_L001_R2_001.rc.qual as input file for the qfile parameter.
-#[ERROR]: mothur does not save a current file for report
-#Using current as input file for the report parameter.
-#Unable to open data/references/HMP_MOCK.filter.fasta. Trying output directory data/process/121203/HMP_MOCK.filter.fasta
-#Unable to open current. Trying output directory data/process/121203/current
-#Unable to open data/process/121203/current
-#
-#Using 12 processors.
-#if you use either a qual file or a report file, you have to have both.
-#[ERROR]: did not complete seq.error.
-#
-#    mothur > quit()
-#    rm: cannot remove `data/process/121203/Mock1_S1_L001_R2_001.align': No such file or directory
-#    rm: cannot remove `data/process/121203/Mock1_S1_L001_R2_001.filter.fasta': No such file or directory
-#    rm: cannot remove `data/process/121203/Mock1_S1_L001_R2_001.align.report': No such file or directory
