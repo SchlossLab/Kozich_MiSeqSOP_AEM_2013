21b58934b6a2f7cbc46cb648d3588a300238e0c0
Pat Schloss <pschloss@umich.edu>
2014-12-24 09:05:29 -0500

    Initial commit

--- code/titrate_deltaq.sh
+++ code/titrate_deltaq.sh
@@ -8,8 +8,9 @@
 # Dependencies...
 # * Paired *.fastq files out of the data/raw/ directory
 #
-# Produces
-# * Contigs formed between pairs of reads
+# Produces contigs formed between pairs of reads and a report:
+# * *.trim.contigs.fasta
+# * *.trim.contigs.report
 #
 ################################################################################
 
@@ -20,3 +21,10 @@ RAW_DIR=$(echo $F_FASTQ | sed -e s/Mock.*//)
 PROC_DIR=$(echo $RAW_DIR | sed -e s/raw/process/)
 
 mothur "#make.contigs(ffastq=$F_FASTQ, rfastq=$R_FASTQ, outputdir=$PROC_DIR, processors=12)"
+
+#
+#   delete:
+#       data/process/121203/Mock1_S1_L001_R1_001.scrap.contigs.fasta
+#
+
+rm $(echo $F_FASTQ | sed -e s/fastq/scrap.contigs.fasta/)
