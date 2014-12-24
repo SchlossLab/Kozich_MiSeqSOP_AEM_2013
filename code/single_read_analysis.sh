0c8ac4fb719ff51fb74248b0c12df405ea94b36d
Pat Schloss <pschloss@umich.edu>
2014-12-24 08:33:01 -0500

    Fix directory problems

--- code/single_read_analysis.sh
+++ code/single_read_analysis.sh
@@ -11,9 +11,9 @@
 # Produces
 # * The error rates for each individual read
 # * From each read file, we will need...
-#   - *error.summary
-#   - *error.matrix
-#   - *error.quality
+#   - *.error.summary
+#   - *.error.matrix
+#   - *.error.quality
 # * These will be used to make box plots showing the relationship between the
 #   quality scores and error type, the effect of the position in the read on
 #   the error rate, and the substitution matrix
@@ -23,24 +23,24 @@
 FASTA=$1
 QUAL=$(echo $FASTA | sed -e s/fasta/qual/)
 
-#need to get path to this folder
-OUTPUT_PATH=$(echo $FASTA | sed -e s/Mock.*fasta//)
+FINAL_FASTA=$(echo $FASTA | sed -e s/raw/process/)
 
+#need to get path to this folder
+OUTPUT_PATH=$(echo $FINAL_FASTA | sed -e s/Mock.*fasta//)
 mkdir -p $OUTPUT_PATH
 
 if [[ $FASTA == *"R1"* ]]; then
     mothur "#set.dir(output=$OUTPUT_PATH);
-        align.seqs(fasta=$FASTA, reference=../data/references/HMP_MOCK.align, processors=12);
-        filter.seqs(fasta=current-../data/references/HMP_MOCK.align, vertical=T);
+        align.seqs(fasta=$FASTA, reference=data/references/HMP_MOCK.align, processors=12);
+        filter.seqs(fasta=current-data/references/HMP_MOCK.align, vertical=T);
         seq.error(fasta=current, qfile=$QUAL, reference=HMP_MOCK.filter.fasta);"
 else
     mothur "#set.dir(output=$OUTPUT_PATH);
         reverse.seqs(fasta=$FASTA, qfile=$QUAL);
-        align.seqs(fasta=current, reference=../data/references/HMP_MOCK.align, processors=12);
-        filter.seqs(fasta=current-../data/references/HMP_MOCK.align, vertical=T);
+        align.seqs(fasta=current, reference=data/references/HMP_MOCK.align, processors=12);
+        filter.seqs(fasta=current-data/references/HMP_MOCK.align, vertical=T);
         seq.error(fasta=current, qfile=current, reference=HMP_MOCK.filter.fasta);"
 fi
 
-PROCFASTA=$(echo $FASTA | sed -e s/raw/process/)
-rm $(echo $PROCFASTA | sed -e s/fasta/align/)
-rm $(echo $PROCFASTA | sed -e s/fasta/filter.fasta/)
+rm $(echo $FINAL_FASTA | sed -e s/fasta/align/)
+rm $(echo $FINAL_FASTA | sed -e s/fasta/filter.fasta/)
