dbde89f7e88ad3d48e04f423b9d24c1c8db0841d
Pat Schloss <pschloss@umich.edu>
2015-01-12 13:28:55 -0500

    Update code to start at precluster files

--- code/get_summary_from_precluster.sh
+++ code/get_summary_from_precluster.sh
@@ -2,15 +2,16 @@
 
 ################################################################################
 #
-# run_mothur_regular.sh
+# get_summary_from_precluster.sh.sh
 #
 #
-# Here we run the *contigs.fasta files through mothur like we would in a normal
-# study
+# Here we run the *precluster.fasta and *precluster.names files through mothur
+# like we would in a normal study to get a sense of the number of OTUs we would
+# observe
 #
 # Dependencies...
-# * A *.contigs.fasta file as input
-# * The data/references/start_stop.positions file
+# * *.precluster.fasta
+# * *.precluster.names
 #
 # Produces...
 # * OTU assignments for each of the three regions
@@ -21,56 +22,47 @@
 #
 ################################################################################
 
-
+FASTA=$1
+NAMES=$2
 
 # here are the pretty standard mothur commands for sequence processing, 
 # pre-clustering, chimera removal, clustering into OTUs, and determining the
 # number of OTUs
 run_mothur () {
-	ALIGN=$1
-	NAMES=$2
-	REGION=$3
+	PRECLUSTER_FASTA=$1
+	PRECLUSTER_NAMES=$2
 	
-	mothur "#filter.seqs(fasta=$ALIGN, vertical=T, trump=., processors=12);
-		unique.seqs(fasta=current, name=$NAMES);
-		pre.cluster(fasta=current, name=current, diffs=2);
-		chimera.uchime(fasta=current, name=current);
+	mothur "chimera.uchime(fasta=$PRECLUSTER_FASTA, name=$PRECLUSTER_NAMES);
 		remove.seqs(fasta=current, name=current, accnos=current);
 		dist.seqs(fasta=current, cutoff=0.20, processors=8);
 		cluster(column=current, name=current);
 		summary.single(rabund=current, calc=nseqs-sobs, label=0.03, subsample=5000)"
 	
 	
-	touch $(echo $CONTIG_FASTA | sed 's/.fasta//').$REGION.filter.unique.precluster.pick.an.ave-std.summary
-	touch $(echo $CONTIG_FASTA | sed 's/.fasta//').$REGION.filter.unique.precluster.pick.an.summary
+	touch $(echo $PRECLUSTER_FASTA | sed 's/.fasta//').pick.an.ave-std.summary
+	touch $(echo $PRECLUSTER_FASTA | sed 's/.fasta//').pick.an.summary
 }
 
 
 # run the function on the three datasets...
-run_mothur $V4_ALIGN $V4_NAMES v4
-run_mothur $V34_ALIGN $V34_NAMES v34
-run_mothur $V45_ALIGN $V45_NAMES v45
+run_mothur $FASTA $NAMES
 
 
 
 # garbage collection
-CONTIG_STUB=$(echo $CONTIG_FASTA | sed -E s/.fasta//)
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.an.list
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.an.rabund
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.an.sabund
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.dist
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.uchime.chimeras
-rm $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.uchime.accnos
+PRECLUSTER_STUB=$(echo $FASTA | sed -E s/.fasta//)
+rm $PRECLUSTER_STUB.pick.an.list
+rm $PRECLUSTER_STUB.pick.an.rabund
+rm $PRECLUSTER_STUB.pick.an.sabund
+rm $PRECLUSTER_STUB.pick.dist
+rm $PRECLUSTER_STUB.uchime.chimeras
+rm $PRECLUSTER_STUB.uchime.accnos
 
 
 # keeping...
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.an.ave-std.summary	#the number of OTUs w/ rarefaction to 5000 seqs
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.an.summary	#the number of OTUs and sequences w/o rarefaction
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.fasta	#the unique, filtered, region-specific alignment, post preclustering, and chimera removal
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.pick.names	#the unique, filtered, region-specific alignment, post preclustering, and chimera removal
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.names #the unique, filtered, region-specific alignment, post preclustering
-# $CONTIG_STUB.v{34,4,45}.filter.unique.precluster.fasta #the unique, filtered, region-specific alignment, post preclustering
-# $CONTIG_STUB.v{34,4,45}.filter.names		#the unique, filtered, region-specific alignment
-# $CONTIG_STUB.v{34,4,45}.filter.unique.fasta #the unique, filtered, region-specific alignment
-# $CONTIG_STUB.report	#from make.contigs - not create here
-# $CONTIG_STUB.fasta	#from make.contigs - not create here
+# $PRECLUSTER_STUB.pick.an.ave-std.summary	#the number of OTUs w/ rarefaction to 5000 seqs
+# $PRECLUSTER_STUB.pick.an.summary			#the number of OTUs and sequences w/o rarefaction
+# $PRECLUSTER_STUB.pick.fasta				#the unique, filtered, region-specific alignment, post preclustering, and chimera removal
+# $PRECLUSTER_STUB.pick.names				#the unique, filtered, region-specific alignment, post preclustering, and chimera removal
+# $PRECLUSTER_STUB.v{34,4,45}.names 		#the unique, filtered, region-specific alignment, post preclustering
+# $PRECLUSTER_STUB.v{34,4,45}.fasta 		#the unique, filtered, region-specific alignment, post preclustering
