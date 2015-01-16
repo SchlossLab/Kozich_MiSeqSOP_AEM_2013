37e438d960d0835c43419927e6dd1464ed09ce44
Pat Schloss <pschloss@umich.edu>
2015-01-16 13:12:40 -0500

    Initial comit

---
+++ code/plot_nmds.R
@@ -0,0 +1,15 @@
+################################################################################
+#
+# plot_nmds.R
+#
+# Here we take in the *.nmds.axes file from the mouse stability analysis and
+# plot it in R as we did in Figure 3 of Kozich et al.
+#
+#
+# Dependencies...
+# * data/process/*/*.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.an.unique_list.thetayc.0.03.lt.ave.nmds.axes
+#
+# Produces...
+# * results/figures/*.nmds.png
+#
+################################################################################
