dad8f5d0a7e31c3f031b439324f1e5a68982500e
Pat Schloss <pschloss@umich.edu>
2015-01-19 12:44:47 -0500

    Change code to use deltaq.error.summary files

--- code/build_table2.R
+++ code/build_table2.R
@@ -1,46 +1,11 @@
+get_delta_q_error <- function(run, region, dq){
 
+    file_name <- paste0("data/process/", run, "/deltaq.error.summary")
+    file <- read.table(file=file_name, header=T)
 
-get_dq_errors <- function(run, delta_q){
-
-    path <- paste0("data/process/", run, "/")
-    mocks <- c("Mock1_S1", "Mock2_S2", "Mock3_S3")
-
-    error_summary_fname <- paste0("data/process/", run, "/", mocks, "_L001_R1_001.", delta_q, ".contigs.filter.error.summary")
-    error_summary <- read.table(file=error_summary_fname[1], header=T, row.names=1)
-    error_summary <- rbind(error_summary, read.table(file=error_summary_fname[2], header=T, row.names=1))
-    error_summary <- rbind(error_summary, read.table(file=error_summary_fname[3], header=T, row.names=1))
-
-    region_fname <- paste0("data/process/", run, "/", mocks, "_L001_R1_001.", delta_q, ".contigs.region")
-    region <- read.table(file=region_fname[1], header=F, row.names=1)
-    region <- rbind(region, read.table(file=region_fname[2], header=F, row.names=1))
-    region <- rbind(region, read.table(file=region_fname[3], header=F, row.names=1))
-
-    good <- error_summary$numparents == 1 & error_summary$ambig == 0
-
-    error_summary.good <- error_summary[good,]
-    region.good <- region[good,]
-
-    aggregate(error_summary.good$error, by=list(region.good), function(x){c(mean(x), length(x))})
+    file[file$deltaQ == dq & file$region == region, "error.rate"]
 }
 
-
-
-merge_delta_q_data <- function(runs, delta_q_data){
-
-    n_tables <- length(delta_q_data)
-    merge_table <- data.frame()
-    for(i in 1:n_tables){
-        n_regions <- nrow(delta_q_data[[i]])
-        mini_table <- data.frame(run=rep(runs[i], n_regions),
-                                 region=delta_q_data[[i]][["Group.1"]],
-                                 error=delta_q_data[[i]][,"x"][,1],
-                                 n_seqs=delta_q_data[[i]][,"x"][,2])
-
-        merge_table <- rbind(merge_table, mini_table)
-    }
-
-    merge_table[order(merge_table$region, merge_table$run),]
-}
 
 
 get_pc_errors <- function(run, region){
@@ -48,7 +13,9 @@ get_pc_errors <- function(run, region){
     path <- paste0("data/process/", run, "/")
     mocks <- c("Mock1_S1", "Mock2_S2", "Mock3_S3")
 
-    error_summary_fname <- paste0("data/process/", run, "/", mocks, "_L001_R1_001.6.contigs.", region, ".filter.unique.precluster.error.summary")
+    error_summary_fname <- paste0("data/process/", run, "/", mocks,
+                                "_L001_R1_001.6.contigs.", region,
+                                ".filter.unique.precluster.error.summary")
     error_summary <- read.table(file=error_summary_fname[1], header=T, row.names=1)
     error_summary <- rbind(error_summary, read.table(file=error_summary_fname[2], header=T, row.names=1))
     error_summary <- rbind(error_summary, read.table(file=error_summary_fname[3], header=T, row.names=1))
@@ -59,7 +26,21 @@ get_pc_errors <- function(run, region){
 
     nseqs <- sum(error_summary.good$weight)
     error <- sum(error_summary.good$weight * error_summary.good$error) / nseqs
-    c(error=error, n_seqs=nseqs)
+    error
+}
+
+
+
+
+get_per_reads_remaining <- function(run, region, dq_a=0, dq_b=6){
+
+    file_name <- paste0("data/process/", run, "/deltaq.error.summary")
+    file <- read.table(file=file_name, header=T)
+
+    basic <- file[file$region == region & file$deltaQ == dq_a, "nseqs"]
+    clean <- file[file$region == region & file$deltaQ == dq_b, "nseqs"]
+
+    100 * clean / basic
 }
 
 
@@ -68,7 +49,9 @@ get_nochim_otus <- function(run, region){
     path <- paste0("data/process/", run, "/")
     mocks <- c("Mock1_S1", "Mock2_S2", "Mock3_S3")
 
-    ave_std_summary <- paste0("data/process/", run, "/", mocks, "_L001_R1_001.6.contigs.", region, ".filter.unique.precluster.perfect.an.ave-std.summary")
+    ave_std_summary <- paste0("data/process/", run, "/", mocks,
+                                "_L001_R1_001.6.contigs.", region,
+                                ".filter.unique.precluster.perfect.an.ave-std.summary")
 
     data_there <- file.info(ave_std_summary)$size != 0
     n_files <- sum(data_there)
@@ -99,7 +82,9 @@ get_typical_otus <- function(run, region){
                     "Mouse1_S10", "Mouse2_S11", "Mouse3_S12")
     type <- gsub("\\d.*", "", libraries)
 
-    ave_std_summary <- paste0("data/process/", run, "/", libraries, "_L001_R1_001.6.contigs.", region, ".filter.unique.precluster.pick.an.ave-std.summary")
+    ave_std_summary <- paste0("data/process/", run, "/", libraries,
+                                "_L001_R1_001.6.contigs.", region,
+                                ".filter.unique.precluster.pick.an.ave-std.summary")
 
     data_there <- file.info(ave_std_summary)$size != 0
     n_files <- sum(data_there)
@@ -133,29 +118,29 @@ get_typical_otus <- function(run, region){
 #error_run <- grepl("data/process/1", folders)
 #error_folders <- folders[error_run]
 #runs <- gsub("data/process/", "", error_folders)
-runs <- c("121203", "121205", "121207")
+runs <- c("121205", "121207")
 regions <- c("v34", "v4", "v45")
 runs_regions <- expand.grid(runs=runs, regions=regions)
 
 
 #Basic error rate
-delta_0 <- lapply(runs, get_dq_errors, delta_q=0)
-delta_0_table <- merge_delta_q_data(runs, delta_0)
+delta_0_errors <- mapply(get_delta_q_error, runs_regions$runs, runs_regions$regions, dq=0)
+delta_0_table <- cbind(runs_regions, delta_0=delta_0_errors)
 
 
 #deltaQ=6 error rate
-delta_6 <- lapply(runs, get_dq_errors, delta_q=6)
-delta_6_table <- merge_delta_q_data(runs, delta_6)
+delta_6_errors <- mapply(get_delta_q_error, runs_regions$runs, runs_regions$regions, dq=6)
+delta_6_table <- cbind(runs_regions, delta_6=delta_6_errors)
 
 
 #Precluster error rate
-pc_report <- mapply(get_pc_errors, run=runs_regions$runs, region=runs_regions$regions)
-pc_table <- cbind(runs_regions, t(pc_report))
+pc_errors <- mapply(get_pc_errors, run=runs_regions$runs, region=runs_regions$regions)
+pc_table <- cbind(runs_regions, pc_errors)
 
 
 #% reads remaining from basic (deltaQ=0)
-per_reads_remaining <- 100 * delta_6_table$n_seqs / delta_0_table$n_seqs
-per_reads_table <- cbind(runs_regions, t(per_reads_remaining))
+per_reads_remaining <- mapply(get_per_reads_remaining, run=runs_regions$runs, region=runs_regions$regions)
+per_reads_table <- cbind(runs_regions, per_reads_remaining)
 
 
 #Mock^b - Sobs with perfect chimera removal
